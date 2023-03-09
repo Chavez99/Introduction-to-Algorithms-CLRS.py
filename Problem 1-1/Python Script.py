0a33f176827a0bcb0274eb519eb4bd93abb0b215
MasterSite <a17491021@itmexicali.edu.mx>
2023-03-08 20:44:42 -0800

    First commmit

---
+++ Problem 1-1/Python Script.py
@@ -0,0 +1,202 @@
+# %%
+from math import *
+N=1
+n=1
+
+t= 3.16*pow(10,15) 
+while n * log(n, 2) < t:
+
+    N += 1 
+    def findFactSum(N):
+        f = 1
+        Sum = 0
+        for i in range(1, N + 1):
+            f = f * i
+            Sum += f
+        return Sum
+    n = findFactSum(N)
+
+while n * log(n, 2) > t:
+   
+    n -= 1
+    while n * log(n, 2) > t:
+        s=7
+        n-=pow(10,s)
+        #s+=1
+#Scientific Notation
+print("{:e}".format(n))
+
+# %%
+from math import *
+N=1
+n=1
+
+t= 3.15*pow(10,13) 
+
+while n * log(n, 2) < t:
+
+    N += 1 
+    def findFactSum(N):
+        f = 1
+        Sum = 0
+        for i in range(1, N + 1):
+            f = f * i
+            Sum += f
+        return Sum
+    n = findFactSum(N)
+
+while n * log(n, 2) > t:
+   
+    n -= 1
+    while n * log(n, 2) > t:
+        s=5
+        n-=pow(10,s)
+        #s+=1
+#Scientific Notation
+print("{:e}".format(n))
+
+# %%
+from math import *
+N=1
+n=1
+
+t= 2.59*pow(10,12) 
+
+while n * log(n, 2) < t:
+
+    N += 1 
+    def findFactSum(N):
+        f = 1
+        Sum = 0
+        for i in range(1, N + 1):
+            f = f * i
+            Sum += f
+        return Sum
+    n = findFactSum(N)
+
+while n * log(n, 2) > t:
+   
+    n -= 1
+    while n * log(n, 2) > t:
+        s=4
+        n-=pow(10,s)
+        #s+=1
+#Scientific Notation
+print("{:e}".format(n))
+
+# %%
+from math import *
+N=1
+n=1
+
+t= 8.64*pow(10,10) 
+
+while n * log(n, 2) < t:
+
+    N += 1 
+    def findFactSum(N):
+        f = 1
+        Sum = 0
+        for i in range(1, N + 1):
+            f = f * i
+            Sum += f
+        return Sum
+    n = findFactSum(N)
+
+while n * log(n, 2) > t:
+   
+    n -= 1
+    while n * log(n, 2) > t:
+        s=3
+        n-=pow(10,s)
+        #s+=1
+#Scientific Notation
+print("{:e}".format(n))
+
+# %%
+from math import *
+N=1
+n=1
+
+t= 3.6*pow(10,9) 
+
+while n * log(n, 2) < t:
+
+    N += 1 
+    def findFactSum(N):
+        f = 1
+        Sum = 0
+        for i in range(1, N + 1):
+            f = f * i
+            Sum += f
+        return Sum
+    n = findFactSum(N)
+
+while n * log(n, 2) > t:
+   
+    n -= 1
+    while n * log(n, 2) > t:
+        s=2
+        n-=pow(10,s)
+        #s+=1
+#Scientific Notation
+print("{:e}".format(n))
+
+# %%
+from math import *
+N=1
+n=1
+
+t= 6*pow(10,7) 
+
+while n * log(n, 2) < t:
+
+    N += 1 
+    def findFactSum(N):
+        f = 1
+        Sum = 0
+        for i in range(1, N + 1):
+            f = f * i
+            Sum += f
+        return Sum
+    n = findFactSum(N)
+
+while n * log(n, 2) > t:
+   
+    n -= 1
+    while n * log(n, 2) > t:
+        s=1
+        n-=pow(10,s)
+        #s+=1
+#Scientific Notation
+print("{:e}".format(n))
+
+# %%
+from math import *
+N=1
+n=1
+t= 1*pow(10,6)  
+
+while n * log(n, 2) < t:
+
+    N += 1 
+    def findFactSum(N):
+        f = 1
+        Sum = 0
+        for i in range(1, N + 1):
+            f = f * i
+            Sum += f
+        return Sum
+    n = findFactSum(N)
+
+while n * log(n, 2) > t:
+   
+    n -= 1
+        #s+=1
+#Scientific Notation
+print("{:e}".format(n))
+
+# %%
+
+
+
